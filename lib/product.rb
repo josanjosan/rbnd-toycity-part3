13a980fc5cbc1603cfc7a9699fb51974e11bd09d
José Robles <joseroblesoneto@gmail.com>
2016-04-14 22:18:33 -0300

    Initialize method calls add_product private method, find_by_title() takes title and returns product that matches a given titl

--- lib/product.rb
+++ lib/product.rb
@@ -8,20 +8,8 @@ class Product
 		@title = options[:title]
 		@price = options[:price]
 		@stock = options[:stock]
-		name_taken = false
-
-		@@products.each do |product|
-			if product.title == options[:title]
-				name_taken = true
-			end
-		end
 		
-		if name_taken == false
-			@@products << self
-		else
-			puts "DuplicateProductError: the product \"#{options[:title]}\" already exists." #WORKS
-			#raise DuplicateProductError, "the product \"#{options[:title]}\" already exists." #DOESN'T WORK
-		end
+		add_product(self)
 
 	end
 
@@ -29,5 +17,34 @@ class Product
 		@@products
 	end
 
+	def self.find_by_title(some_title)
+		product_found = @@products.select { |product| product.title == some_title }
+		return product_found[0]
+	end
+
+	def in_stock?
+		self.stock > 0
+	end
+
 	private
+
+	def add_product(new_product)
+		
+		name_taken = false
+		
+		@@products.each do |product|
+			if product.title == new_product.title
+				name_taken = true
+			end
+		end
+		
+		if name_taken == false
+			@@products << new_product
+		else
+			puts "DuplicateProductError: the product \"#{self.title}\" already exists." #WORKS
+			#raise DuplicateProductError, "the product \"#{options[:title]}\" already exists." #DOESN'T WORK
+		end
+
+	end
+
 end
